e3686c59f9da67bc46f26728abd91563f1e4fad1
Siddhant Tomar <76844796+siddhant-debug@users.noreply.github.com>
2026-03-16 17:22:31 +0000

    feat: add episodic memory table and unify history

--- backend/alembic/versions/92981f18fcc9_merge_multiple_heads.py
+++ backend/alembic/versions/92981f18fcc9_merge_multiple_heads.py
@@ -6,23 +6,17 @@ Create Date: 2026-03-11 12:59:02.383843
 
 """
 from typing import Sequence, Union
-
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-revision: str = '92981f18fcc9'
-down_revision: Union[str, Sequence[str], None] = ('2051721b9f4d', 'fcc17382d5a0')
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
-
+revision = '92981f18fcc9'
+down_revision = ('2051721b9f4d', 'fcc17382d5a0')
+branch_labels = None
+depends_on = None
 
 def upgrade() -> None:
-    """Upgrade schema."""
     pass
 
-
 def downgrade() -> None:
-    """Downgrade schema."""
     pass
